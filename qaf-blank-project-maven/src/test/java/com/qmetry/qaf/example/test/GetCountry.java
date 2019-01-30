5a0245060f9dd78430bb150f5c653b2e62771d16
Hidayath-S <hiddu91@gmail.com>
2019-01-30 21:52:06 +0530

    updated json path

--- qaf-blank-project-maven/src/test/java/com/qmetry/qaf/example/test/GetCountry.java
+++ qaf-blank-project-maven/src/test/java/com/qmetry/qaf/example/test/GetCountry.java
@@ -1,15 +1,20 @@
 package com.qmetry.qaf.example.test;
 
 import org.testng.annotations.Test;
+
+import com.qmetry.qaf.automation.core.ConfigurationManager;
 import com.qmetry.qaf.automation.step.WsStep;
 
 public class GetCountry {
 	@Test()
 	public void getCountry(){
 		
-		WsStep.userRequests("groupkt.call");
+		WsStep.userRequests("getAllCountry.call");
 		WsStep.responseShouldHaveStatusCode(200);
-		
+		WsStep.sayValueAtJsonPath("Country", "$.RestResponse.result[?(@.alpha2_code=='IN')].name");
+		//WsStep.responseShouldHaveValueAtJsonpath("India", "$.RestResponse.result[?(@.alpha2_code=='IN')].name");
+		String countryName=ConfigurationManager.getBundle().getString("Country");
+		System.out.println("countryName="+countryName);
 	}
 
 }
