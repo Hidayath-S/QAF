010badde456f6ebd731535d567724b7a9a2b2dbf
Hidayath-S <hiddu91@gmail.com>
2020-09-20 21:45:21 +0530

    updates

--- QAF-API-Testing/src/main/java/com/swagger/parser/CreateBDDScenario.java
+++ QAF-API-Testing/src/main/java/com/swagger/parser/CreateBDDScenario.java
@@ -3,6 +3,7 @@ package com.swagger.parser;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -45,30 +46,48 @@ public class CreateBDDScenario {
 		XSSFSheet sheet = workbook.getSheetAt(0);
 		int rowCount = sheet.getLastRowNum() + 1;
 		for (int i = 1; i < rowCount; i++) {
+			String path = sheet.getRow(i).getCell(0).getStringCellValue();
 			String operationId = sheet.getRow(i).getCell(1).getStringCellValue();
 			String method = sheet.getRow(i).getCell(2).getStringCellValue();
-			String resDefName = bddScenario.getResponseFileName(swgFile, excelFilePath, method);
-			System.out.println(method);
-			JSONParser jsonParser = new JSONParser();
-			FileReader fileReader = new FileReader(defPath + "\\" + resDefName + ".json");
-			String json = jsonParser.parse(fileReader).toString();
-			List<String> jsonPaths = parseSwagger.generateJSONPaths(json);
-			for (int j = 0; j < jsonPaths.size(); j++) {
-				String path = jsonPaths.get(j).toString();
-				String value = path.replace("$", "").replace(".", "").replace("[", "").replace("]", "");
+			String resDefName = bddScenario.getResponseFileName(swgFile, excelFilePath, method, path);
+			System.out.println(resDefName);
+			if (resDefName.equalsIgnoreCase("")) {
+				System.out.println("There is no success response defined for operationId " + operationId);
+			} else {
+				JSONParser jsonParser = new JSONParser();
+				FileReader fileReader = new FileReader(defPath + "\\" + resDefName + ".json");
+				String json = jsonParser.parse(fileReader).toString();
+				List<String> jsonPaths = parseSwagger.generateJSONPaths(json);
+				List pathValidationList=new ArrayList<>();
+				String pathValidationMethods="";
+				for (int j = 0; j < jsonPaths.size(); j++) {
+					String jsonPath = jsonPaths.get(j).toString();
+					String value = jsonPath.replace("$", "").replace(".", "").replace("[", "").replace("]", "");
 
-				jsonPathValidation = "AND response has value '${" + value + "}' at jsonpath '" + path + "'";
+					jsonPathValidation = "AND response has value '${" + value + "}' at jsonpath '" + jsonPath + "'\r\n";
+					
+					pathValidationList.add(jsonPathValidation);
+					pathValidationMethods=pathValidationList.toString().replace(",", "").substring(1);
 
-				scenarioContent = "SCENARIO: To test happy path scenario for " + operationId + "  API\r\n"
-						+ "META-DATA: {'desc':'This is an example of scenario using QAF-BDD','groups':['POSITIVE']}\r\n"
+					
+				}
+				pathValidationMethods=pathValidationMethods.substring(0, pathValidationMethods.lastIndexOf("]"));
+				String str1= "SCENARIO: To test happy path scenario for " + operationId + "  API\r\n"
+						+ "META-DATA: {'desc':'This is an example of scenario using QAF-BDD','dataFile':'resources/TestData/petStoreTestData.csv','groups':['POSITIVE']}\r\n"
 						+ "Given COMMENT: '" + operationId + " micro service is up and running' \r\n"
 						+ "When user requests '" + operationId + "'  \r\n"
-						+ "Then response should have status code '200'\r\n" + "AND response has value '${" + value
-						+ "}' at jsonpath '" + path + "'\r\n" + "END\r\n" + "";
+						+ "Then response should have status code '200'\r\n";
+				String str2="END\r\n";
+				StringBuilder builder=new StringBuilder();
+				scenarioContent=builder.append(str1).append(pathValidationMethods).append(str2).toString();
+				
+				
+				
 				FileWriter writer = new FileWriter(bddFilePath + "\\" + operationId + ".BDD");
 				writer.write(scenarioContent);
 				writer.flush();
 				writer.close();
+
 			}
 
 		}
@@ -77,132 +96,113 @@ public class CreateBDDScenario {
 
 	}
 
-	public String getResponseFileName(String swgFilePath, String excelsheetPath, String method) throws Exception {
+	public String getResponseFileName(String swgFilePath, String excelsheetPath, String method, String path)
+			throws Exception {
 		Swagger swagger = new SwaggerParser().read(swgFilePath);
-		FileInputStream inputStream = new FileInputStream(excelsheetPath);
-		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-		XSSFSheet sheet = workbook.getSheetAt(0);
 		String resDefName = "";
 		Response rsp;
-		int rownum = sheet.getLastRowNum() + 1;
-		Map<String, Path> allPaths=swagger.getPaths();
+		Map<String, Path> allPaths = swagger.getPaths();
+		if (method.equalsIgnoreCase("GET")) {
+			Collection<String> response = allPaths.get(path).getGet().getResponses().keySet();
+			Iterator<String> rspIt = response.iterator();
+			while (rspIt.hasNext()) {
+				String b = rspIt.next();
 
-		for (int i = 0; i < rownum; i++) {
-			String path = sheet.getRow(i).getCell(0).getStringCellValue();
-			String httpMethod = sheet.getRow(i).getCell(2).getStringCellValue();
-			if (httpMethod.equalsIgnoreCase("GET")) {
-				Collection<String> response = allPaths.get(path).getGet().getResponses().keySet();
-				Iterator<String> rspIt = response.iterator();
-				while (rspIt.hasNext()) {
-					String b = rspIt.next();
+				if (b.equals("200")) {
+					// System.out.println(path);
 
-					if (b.equals("200")) {
-						// System.out.println(path);
-
-						rsp = swagger.getPath(path).getGet().getResponses().get(b);
-						if (rsp.getResponseSchema().getClass().getName()
-								.equalsIgnoreCase("io.swagger.models.ArrayModel")) {
-							ArrayModel arrayModel = (ArrayModel) rsp.getResponseSchema();
-							RefProperty properties = (RefProperty) arrayModel.getItems();
-							resDefName = properties.getSimpleRef();
-						}
-						if (rsp.getResponseSchema().getClass().getName()
-								.equalsIgnoreCase("io.swagger.models.RefModel")) {
-							RefModel arrayModel = (RefModel) rsp.getResponseSchema();
-							String properties = arrayModel.getSimpleRef();
-							resDefName = properties;
-						}
-						
+					rsp = swagger.getPath(path).getGet().getResponses().get(b);
+					if (rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.ArrayModel")) {
+						ArrayModel arrayModel = (ArrayModel) rsp.getResponseSchema();
+						RefProperty properties = (RefProperty) arrayModel.getItems();
+						resDefName = properties.getSimpleRef();
+					}
+					if (rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.RefModel")) {
+						RefModel arrayModel = (RefModel) rsp.getResponseSchema();
+						String properties = arrayModel.getSimpleRef();
+						resDefName = properties;
 					}
 
 				}
+
 			}
-			if (httpMethod.equalsIgnoreCase("POST")) {
-				Collection<String> response = allPaths.get(path).getPost().getResponses().keySet();
-				Iterator<String> rspIt = response.iterator();
-				while (rspIt.hasNext()) {
-					String b = rspIt.next();
-
-					if (b.equals("200")) {
-						// System.out.println(path);
-
-						rsp = swagger.getPath(path).getPost().getResponses().get(b);
-						if (rsp.getResponseSchema().getClass().getName()
-								.equalsIgnoreCase("io.swagger.models.ArrayModel")) {
-							ArrayModel arrayModel = (ArrayModel) rsp.getResponseSchema();
-							RefProperty properties = (RefProperty) arrayModel.getItems();
-							resDefName = properties.getSimpleRef();
-						}
-						if (rsp.getResponseSchema().getClass().getName()
-								.equalsIgnoreCase("io.swagger.models.RefModel")) {
-							RefModel arrayModel = (RefModel) rsp.getResponseSchema();
-							String properties = arrayModel.getSimpleRef();
-							resDefName = properties;
-						}
-						
-
-					}
-
-				}
-			}
-			if (httpMethod.equalsIgnoreCase("PUT")) {
-				Collection<String> response = allPaths.get(path).getPut().getResponses().keySet();
-				Iterator<String> rspIt = response.iterator();
-				while (rspIt.hasNext()) {
-					String b = rspIt.next();
-
-					if (b.equals("200")) {
-						// System.out.println(path);
-
-						rsp = swagger.getPath(path).getPut().getResponses().get(b);
-						if (rsp.getResponseSchema().getClass().getName()
-								.equalsIgnoreCase("io.swagger.models.ArrayModel")) {
-							ArrayModel arrayModel = (ArrayModel) rsp.getResponseSchema();
-							RefProperty properties = (RefProperty) arrayModel.getItems();
-							resDefName = properties.getSimpleRef();
-						}
-						if (rsp.getResponseSchema().getClass().getName()
-								.equalsIgnoreCase("io.swagger.models.RefModel")) {
-							RefModel arrayModel = (RefModel) rsp.getResponseSchema();
-							String properties = arrayModel.getSimpleRef();
-							resDefName = properties;
-						}
-						
-					}
-
-				}
-			}
-			if (httpMethod.equalsIgnoreCase("DELETE")) {
-				Collection<String> response = allPaths.get(path).getDelete().getResponses().keySet();
-				Iterator<String> rspIt = response.iterator();
-				while (rspIt.hasNext()) {
-					String b = rspIt.next();
-
-					if (b.equals("200")) {
-						// System.out.println(path);
-
-						rsp = swagger.getPath(path).getDelete().getResponses().get(b);
-						if (rsp.getResponseSchema().getClass().getName()
-								.equalsIgnoreCase("io.swagger.models.ArrayModel")) {
-							ArrayModel arrayModel = (ArrayModel) rsp.getResponseSchema();
-							RefProperty properties = (RefProperty) arrayModel.getItems();
-							resDefName = properties.getSimpleRef();
-						}
-						if (rsp.getResponseSchema().getClass().getName()
-								.equalsIgnoreCase("io.swagger.models.RefModel")) {
-							RefModel arrayModel = (RefModel) rsp.getResponseSchema();
-							String properties = arrayModel.getSimpleRef();
-							resDefName = properties;
-						}
-						
-					}
-
-				}
-			}
-
 		}
-		workbook.close();
-		
+		if (method.equalsIgnoreCase("POST")) {
+			Collection<String> response = allPaths.get(path).getPost().getResponses().keySet();
+			Iterator<String> rspIt = response.iterator();
+			while (rspIt.hasNext()) {
+				String b = rspIt.next();
+
+				if (b.equals("200")) {
+					// System.out.println(path);
+
+					rsp = swagger.getPath(path).getPost().getResponses().get(b);
+					if (rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.ArrayModel")) {
+						ArrayModel arrayModel = (ArrayModel) rsp.getResponseSchema();
+						RefProperty properties = (RefProperty) arrayModel.getItems();
+						resDefName = properties.getSimpleRef();
+					}
+					if (rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.RefModel")) {
+						RefModel arrayModel = (RefModel) rsp.getResponseSchema();
+						String properties = arrayModel.getSimpleRef();
+						resDefName = properties;
+					}
+
+				}
+
+			}
+		}
+		if (method.equalsIgnoreCase("PUT")) {
+			Collection<String> response = allPaths.get(path).getPut().getResponses().keySet();
+			Iterator<String> rspIt = response.iterator();
+			while (rspIt.hasNext()) {
+				String b = rspIt.next();
+
+				if (b.equals("200")) {
+					// System.out.println(path);
+
+					rsp = swagger.getPath(path).getPut().getResponses().get(b);
+					if (rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.ArrayModel")) {
+						ArrayModel arrayModel = (ArrayModel) rsp.getResponseSchema();
+						RefProperty properties = (RefProperty) arrayModel.getItems();
+						resDefName = properties.getSimpleRef();
+					}
+					if (rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.RefModel")) {
+						RefModel arrayModel = (RefModel) rsp.getResponseSchema();
+						String properties = arrayModel.getSimpleRef();
+						resDefName = properties;
+					}
+
+				}
+
+			}
+		}
+		if (method.equalsIgnoreCase("DELETE")) {
+			Collection<String> response = allPaths.get(path).getDelete().getResponses().keySet();
+			Iterator<String> rspIt = response.iterator();
+			while (rspIt.hasNext()) {
+				String b = rspIt.next();
+
+				if (b.equals("200")) {
+					// System.out.println(path);
+
+					rsp = swagger.getPath(path).getDelete().getResponses().get(b);
+					if (rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.ArrayModel")) {
+						ArrayModel arrayModel = (ArrayModel) rsp.getResponseSchema();
+						RefProperty properties = (RefProperty) arrayModel.getItems();
+						resDefName = properties.getSimpleRef();
+					}
+					if (rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.RefModel")) {
+						RefModel arrayModel = (RefModel) rsp.getResponseSchema();
+						String properties = arrayModel.getSimpleRef();
+						resDefName = properties;
+					}
+
+				}
+
+			}
+		}
+
 		return resDefName;
 	}
 }
--- QAF-API-Testing/src/main/java/com/swagger/parser/Dummy.java
+++ QAF-API-Testing/src/main/java/com/swagger/parser/Dummy.java
@@ -23,46 +23,9 @@ public class Dummy {
 
 	public static void main(String[] args){
 		try{
-			Swagger swagger = new SwaggerParser().read("C:/Users/Hidayath/Desktop/yaml/petStore/Swagger2.0/petStoreV2.json");
-			FileInputStream inputStream = new FileInputStream("C:/Users/Hidayath/Desktop/APIDetails.xlsx");
-			XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-			XSSFSheet sheet = workbook.getSheetAt(0);
-			int rownum = sheet.getLastRowNum() + 1;
-			for (int i = 0; i < rownum; i++) {
-				String path=sheet.getRow(i).getCell(0).getStringCellValue();
-				String httpMethod = sheet.getRow(i).getCell(2).getStringCellValue();
-				if(httpMethod.equalsIgnoreCase("GET")){
-					Collection<String> response=swagger.getPath(path).getGet().getResponses().keySet();
-					Iterator<String> rspIt=response.iterator();
-					while(rspIt.hasNext()){
-						String b=rspIt.next();
-						
-						if(b.equals("200")){
-							//System.out.println(path);
-							
-							Response rsp = swagger.getPath(path).getGet().getResponses().get(b);
-							if(rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.ArrayModel")){
-								ArrayModel arrayModel=(ArrayModel) rsp.getResponseSchema();
-								RefProperty properties=(RefProperty) arrayModel.getItems();
-								System.out.println(properties.getSimpleRef());
-							}
-							if(rsp.getResponseSchema().getClass().getName().equalsIgnoreCase("io.swagger.models.RefModel")){
-								RefModel arrayModel= (RefModel) rsp.getResponseSchema();
-								String properties=arrayModel.getSimpleRef();
-								System.out.println(properties);
-							}
-							
-							
-							
-							
-						}
-						
-					}	
-				}
-				
-				
-				
-			}
+			Swagger swagger = new SwaggerParser().read("C:\\Users\\Hidayath\\Desktop\\yaml\\petStore\\swagger3.0\\petStore.json");
+			System.out.println(swagger.getSwagger());
+			
 			
 		}
 		catch(Exception e){
